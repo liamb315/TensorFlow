9cea01392d7702b32839827bb2e3afbff2057c40
William Fedus <liam.fedus@gmail.com>
2016-05-26 13:58:49 -0700

    Tensorboard graph evaluaton

--- gan/train_gan_new.py
+++ gan/train_gan_new.py
@@ -111,7 +111,7 @@ def train_generator(gan, args, sess, train_writer, initial_load = True):
 		# state_gen = gan.initial_state_gen.eval()
 		# state_dis = gan.initial_state_dis.eval()
 
-		for batch in xrange(50):
+		for batch in xrange(25):
 		# for batch in xrange(batcher.num_batches):
 			start = time.time()
 			x, _  = batcher.next_batch()
